faddf15aa07025a0f991d3e6bee8c0ea55217129
Christian Tarta <chris.j.tarta@gmail.com>
2023-08-21 15:56:44 -0700

    Moved from .ts to .js and fixed some issues for Tasks 2, 3, 4

--- cypress/e2e/Christian_Tests/Task_4.cy.js
+++ cypress/e2e/Christian_Tests/Task_4.cy.js
@@ -1,22 +1,19 @@
 // @requires chromeWebSecurity = false in cypress.config and last test in file
 it ('Task 4 (Christian)', () => {
     const tmout = 15000;
-    const id = 'zyxw-abcd-6789';
+    const id = 'BFSA-FKRK-1189';
     cy.visit('https://dev.storibox.com');
     cy.get('#scanCardInput').type(id + '{Enter}');
-    cy.get('#attendeeName').then((name) => {
-        if (name.is(':visible')) {
-            cy.wrap(name).type('Bob');
-        }
-    });
-    cy.get('#attendeeEmail').then((email) => {
-        if (email.is(':visible')) {
-            cy.wrap(email).type('foo@bar.com');
-        }
-    });
-    cy.get('#saveAttEmail').then((GO) => {
-        if (GO.is(':visible')) {
-            cy.wrap(GO).click();
+    cy.wait(1000);
+    cy.get('body').then((body) => {
+        if (body.find('#attendeeName').length > 0) {
+            cy.get('#attendeeName').then((name) => {
+                if (name.is(':visible')) {
+                    cy.wrap(name).type('Bob');
+                    cy.get('#attendeeEmail').type('foo@bar.com');
+                    cy.get('#saveAttEmail').click();
+                }
+            });
         }
     });
     cy.intercept('https://www.sandbox.paypal.com/smart/api/**').as('paypal');
