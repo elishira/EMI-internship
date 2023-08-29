dd387df2896e394b10d901cdcd63f8637ac6f84f
elishira <97641486+elishira@users.noreply.github.com>
2023-08-28 22:07:11 -0700

    Update Task_4.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_4.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_4.cy.js
@@ -1,12 +1,11 @@
-// works
 it('Occo.io Case 6: Date Range -> Last 7 Days', () => {
-    cy.authAdminConsole();
-    cy.get('#select2-catSelId-container').type('etes{Enter}');
-    cy.wait(2000);
-    cy.get('#filterDates').click();
-    cy.wait(2000);
-    cy.get('.ranges > ul > [data-range-key="Last 7 Days"]')
-      .click({ multiple: true, force: true });
-    cy.contains('Apply').click();
-    cy.url().should('contain', 'range=Last7Days');
+  cy.on('uncaught:exception', () => { cy.wait(1000); });
+  cy.authAdminConsole();
+  const tmout = 25000;
+  cy.get('#select2-catSelId-container', { timeout: tmout }).type('etes{Enter}');
+  cy.get('#filterDates', { timeout: tmout }).click();
+  cy.get('.ranges > ul > [data-range-key="Last 7 Days"]', { timeout: tmout })
+    .click({ multiple: true, force: true });
+  cy.contains('Apply', { timeout: tmout }).click();
+  cy.url().should('contain', 'range=Last7Days', { timeout: tmout });
 });
