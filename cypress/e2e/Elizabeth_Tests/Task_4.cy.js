8fb2b59e2feccd9a89b938c75c9cb18401e5fd8f
elishira <97641486+elishira@users.noreply.github.com>
2023-09-05 01:48:09 -0700

    Update Task_4.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_4.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_4.cy.js
@@ -1,11 +1,52 @@
-it('Occo.io Case 6: Date Range -> Last 7 Days', () => {
+it('occo.io -> test print, share, and download', () => {
   cy.on('uncaught:exception', () => { cy.wait(1000); });
   cy.authAdminConsole();
   const tmout = 25000;
-  cy.get('#select2-catSelId-container', { timeout: tmout }).type('etes{Enter}');
-  cy.get('#filterDates', { timeout: tmout }).click();
-  cy.get('.ranges > ul > [data-range-key="Last 7 Days"]', { timeout: tmout })
-    .click({ multiple: true, force: true });
-  cy.contains('Apply', { timeout: tmout }).click();
-  cy.url().should('contain', 'range=Last7Days', { timeout: tmout });
+  cy.get('#select2-catSelId-container', { timeout: tmout }).click()
+  cy.get('.select2-search__field', { timeout: tmout }).type('elizabeth_event (yrpz){enter}')
+  cy.get('#filterDates', { timeout: tmout }).clear().type('2023-06-21 13:00 - 2023-06-24  13:00{enter}');
+  cy.get('#setFilter', { timeout: tmout }).click();
+  cy.get('#item_6114054 > .resize-icon > img', { timeout: tmout }).click();
+  cy.get('.lg-next', { timeout: tmout }).click();
+  cy.get('.lg-close-custom', { timeout: tmout }).click();
+
+  let printCount;
+  cy.get('#item_6114021 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    printCount = parseInt(text);
+    printCount = printCount + 1;
+  })
+  cy.get('.thumbCheck > #\\36 114021', { timeout: tmout }).click();
+  cy.get('.print-icon-small', { timeout: tmout }).click();
+  cy.get('#print_photo_button', { timeout: tmout }).click();
+  cy.wait(2000);
+  cy.get('#item_6114021 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    const parsedFinalPrintCount = parseInt(text);
+    expect(parsedFinalPrintCount).to.eq(printCount);
+  })
+
+  let shareCount;
+  cy.get('#item_6114020 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    shareCount = parseInt(text);
+    shareCount = shareCount + 1;
+  })
+  cy.get('.thumbCheck > #\\36 114020').click();
+  cy.get('#share > .fas').click();
+  cy.reload();
+  cy.get('#item_6114020 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    const parsedFinalShareCount = parseInt(text);
+    expect(parsedFinalShareCount).to.eq(shareCount);
+  })
+
+  let downloadCount;
+  cy.get('#item_6114019 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    downloadCount = parseInt(text);
+    downloadCount = downloadCount + 1;
+  })
+  cy.get('.thumbCheck > #\\36 114019').click();
+  cy.get('#downloadImage > .fas').click();
+  cy.reload();
+  cy.get('#item_6114019 > .img-statistic > span > .req_count', { timeout: tmout }).invoke('text').then((text) => {
+    const parsedFinalDownloadCount = parseInt(text);
+    expect(parsedFinalDownloadCount).to.eq(downloadCount);
+  })
 });
