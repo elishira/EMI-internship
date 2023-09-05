2cba10f95039b39837a62ae2e6df5be189039831
elishira <97641486+elishira@users.noreply.github.com>
2023-09-05 01:45:56 -0700

    Update Task_1.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_1.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_1.cy.js
@@ -1,23 +1,26 @@
 it('passcode test', () => {
     cy.authAdminConsole();
     const tmout = 25000;
-    cy.get('#templateEventTab > :nth-child(4) > .nav-link', {timeout: tmout}).click();
-    cy.get('#passCodes_table', {timeout: tmout}).find('tr', {timeout: tmout}).its('length').as('initialRowCount')
-    cy.get('#generatePassCodesButton', {timeout: tmout}).click();
-    cy.get('.placeholder', {timeout: tmout}).type('Event1 (etes)');
-    cy.get('ul > :nth-child(1) > label', {timeout: tmout}).click();
-    cy.get('.icon-caret', {timeout: tmout}).click();
-    cy.get('#genAttEmail', {timeout: tmout}).type("eliziko@hotmail.com");
-    cy.get('#genAttCount', {timeout: tmout}).type(10);
-    cy.get('#pcKioskExpMin', {timeout: tmout}).type(10);
-    cy.get('#pcUrlPrefix', {timeout: tmout}).type("https://dev/storibox.com");
-    cy.get('#genAttendeeAct', {timeout: tmout}).click();
-    cy.get('#messagePopUp > .modal-dialog > .modal-content > .modal-footer > .btn', {timeout: tmout}).click();
-    cy.get('#templateEventTab > :nth-child(1) > .nav-link', {timeout: tmout}).click();
-    cy.get('#templateEventTab > :nth-child(4) > .nav-link', {timeout: tmout}).click();
-    cy.get('@initialRowCount', {timeout: tmout}).then((initialRowCount) => {
-        cy.get('#passCodes_table', {timeout: tmout})
-            .find('tr')
-            .should('have.length', initialRowCount + 1, {timeout: tmout})
+    cy.get('#templateEventTab > :nth-child(4) > .nav-link', { timeout: tmout }).click();
+    cy.get('#passCodes_table', { timeout: tmout })
+        .find('tr', { timeout: tmout })
+        .its('length')
+        .as('initialRowCount')
+    cy.get('#generatePassCodesButton', { timeout: tmout }).click();
+    cy.get('.placeholder', { timeout: tmout }).type('Event1 (etes)');
+    cy.get('ul > :nth-child(1) > label', { timeout: tmout }).click();
+    cy.get('.icon-caret', { timeout: tmout }).click();
+    cy.get('#genAttEmail', { timeout: tmout }).type("eliziko@hotmail.com");
+    cy.get('#genAttCount', { timeout: tmout }).type(10);
+    cy.get('#pcKioskExpMin', { timeout: tmout }).type(10);
+    cy.get('#pcUrlPrefix', { timeout: tmout }).type("https://dev/storibox.com");
+    cy.get('#genAttendeeAct', { timeout: tmout }).click();
+    cy.get('#messagePopUp > .modal-dialog > .modal-content > .modal-footer > .btn', { timeout: tmout }).click();
+    cy.get('#templateEventTab > :nth-child(1) > .nav-link', { timeout: tmout }).click();
+    cy.get('#templateEventTab > :nth-child(4) > .nav-link', { timeout: tmout }).click();
+    cy.get('@initialRowCount', { timeout: tmout }).then((initialRowCount) => {
+        cy.get('#passCodes_table', { timeout: tmout })
+            .find('tr', { timeout: tmout })
+            .should('have.length', initialRowCount + 1, { timeout: tmout })
     })
 })
