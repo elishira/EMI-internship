09c1a0a99374bfe227b0df04ad56c090fde4ac30
elishira <97641486+elishira@users.noreply.github.com>
2023-09-05 01:48:51 -0700

    Update Task_5.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_5.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_5.cy.js
@@ -6,24 +6,25 @@ it('case 1 (dev-admin.occo)', () => {
     // get rid of this hardcoded value
     cy.get(':nth-child(53) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
     let activated = false;
-    cy.get('#useRemoveBgBackup').then($checkbox => {
-        if ($checkbox.is(':checked')) {
-            activated = true;
-            cy.log('Checkbox is checked.');
-            cy.log(activated)
-        } else {
-            cy.log('not checked')
-        }
-        cy.get('#useRemoveBgBackup', { timeout: tmout }).click()
-        cy.get('#updateBg', { timeout: tmout }).click()
-        cy.get('#deleteBg_196', { timeout: tmout }).click()
-        cy.get(':nth-child(53) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
+    let pipelineName;
+    cy.get('#bgName').invoke('val').then((text) => {
+        pipelineName = text;
+        cy.get('#useRemoveBgBackup').then($checkbox => {
+            if ($checkbox.is(':checked')) {
+                activated = true;
+            }
+            cy.get('#useRemoveBgBackup', { timeout: tmout }).click()
+            cy.get('#bgName', { timeout: tmout }).type('1')
+            cy.get('#updateBg', { timeout: tmout }).click()
+            cy.get('#deleteBg_196', { timeout: tmout }).click()
 
-
-        if (activated) {
-            cy.get('#useRemoveBgBackup').should('not.be.checked', { timeout: tmout })
-        } else {
-            cy.get('#useRemoveBgBackup').should('be.checked', { timeout: tmout })
-        }
+            cy.get(':nth-child(53) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
+            cy.get('#bgName').invoke('val').should('eq', pipelineName + '1');
+            if (activated) {
+                cy.get('#useRemoveBgBackup').should('not.be.checked', { timeout: tmout })
+            } else {
+                cy.get('#useRemoveBgBackup').should('be.checked', { timeout: tmout })
+            }
+        });
     });
 })
