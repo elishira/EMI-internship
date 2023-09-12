b3d1b35d43e4fac30a5743f802e0ceb772623263
elishira <97641486+elishira@users.noreply.github.com>
2023-09-11 22:07:40 -0700

    Update Task_6.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_6.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_6.cy.js
@@ -3,24 +3,26 @@ it('duplicate pipeline', () => {
     const tmout = 20000;
     cy.get('#cloudProcessing > a', { timeout: tmout }).click()
     cy.get('#bg-list', { timeout: tmout }).find('tr', { timeout: tmout }).its('length').as('initialRowCount')
-    cy.get(':nth-child(53) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
-    cy.get(':nth-child(53) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
+    cy.get(':nth-child(1) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
+    cy.get(':nth-child(1) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
     let pipelineName;
     cy.get('#bgName').invoke('val').then((text) => {
         pipelineName = text;
+        cy.log(pipelineName);
         cy.get('.close').click();
-        cy.get('#deleteBg_196', { timeout: tmout }).click();
-        cy.get(':nth-child(53) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(2) > a', { timeout: tmout }).click()
+        cy.get(':nth-child(1) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
+        cy.get(':nth-child(1) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(2) > a', { timeout: tmout }).click()
         cy.get('#updateBg').click();
         cy.reload();
-        cy.get(':nth-child(54) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
-        cy.get(':nth-child(54) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
+        cy.get(':nth-child(2) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
+        cy.get(':nth-child(2) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
 
         cy.get('#bgName').invoke('val').should('eq', pipelineName + '_copy');
         cy.get('.close').click();
     });
-    cy.get(':nth-child(54) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
-    cy.get(':nth-child(54) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(3) > a', { timeout: tmout }).click()
+    cy.reload();
+    cy.get(':nth-child(2) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
+    cy.get(':nth-child(2) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(3) > a > .text-danger', { timeout: tmout }).click()
     cy.get('@initialRowCount', { timeout: tmout }).then((initialRowCount) => {
         cy.get('#bg-list', { timeout: tmout })
             .find('tr', { timeout: tmout })
