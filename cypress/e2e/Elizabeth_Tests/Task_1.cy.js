d3c557d6b827929412720ae3ad245107c40b2029
elishira <97641486+elishira@users.noreply.github.com>
2023-09-11 22:04:46 -0700

    Update Task_1.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_1.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_1.cy.js
@@ -5,12 +5,20 @@ it('passcode test', () => {
     cy.get('#passCodes_table', { timeout: tmout })
         .find('tr', { timeout: tmout })
         .its('length')
-        .as('initialRowCount')
+        .as('rowCount');
+
+    let initialRowCount;
+    cy.get('#templateEventTab > :nth-child(1) > .nav-link', { timeout: tmout }).click();
+    cy.get('#templateEventTab > :nth-child(4) > .nav-link', { timeout: tmout }).click();
+    cy.get('@rowCount', { timeout: tmout }).then((rowCount) => {
+        initialRowCount = rowCount;
+    })
+
     cy.get('#generatePassCodesButton', { timeout: tmout }).click();
     cy.get('.placeholder', { timeout: tmout }).type('Event1 (etes)');
     cy.get('ul > :nth-child(1) > label', { timeout: tmout }).click();
     cy.get('.icon-caret', { timeout: tmout }).click();
-    cy.get('#genAttEmail', { timeout: tmout }).type("eliziko@hotmail.com");
+    cy.get('#genAttEmail', { timeout: tmout }).type("andranik@occo.io");
     cy.get('#genAttCount', { timeout: tmout }).type(10);
     cy.get('#pcKioskExpMin', { timeout: tmout }).type(10);
     cy.get('#pcUrlPrefix', { timeout: tmout }).type("https://dev/storibox.com");
@@ -18,9 +26,8 @@ it('passcode test', () => {
     cy.get('#messagePopUp > .modal-dialog > .modal-content > .modal-footer > .btn', { timeout: tmout }).click();
     cy.get('#templateEventTab > :nth-child(1) > .nav-link', { timeout: tmout }).click();
     cy.get('#templateEventTab > :nth-child(4) > .nav-link', { timeout: tmout }).click();
-    cy.get('@initialRowCount', { timeout: tmout }).then((initialRowCount) => {
-        cy.get('#passCodes_table', { timeout: tmout })
-            .find('tr', { timeout: tmout })
-            .should('have.length', initialRowCount + 1, { timeout: tmout })
+    cy.get('@rowCount', { timeout: tmout }).then((rowCount) => {
+        expect(initialRowCount + 1).to.equal(rowCount);
     })
+
 })
