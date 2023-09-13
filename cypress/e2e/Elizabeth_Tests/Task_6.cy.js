0188983f17e2cfe8a0c85f655fdba5feb39659d0
elishira <97641486+elishira@users.noreply.github.com>
2023-09-13 12:11:57 -0700

    Update Task_6.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_6.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_6.cy.js
@@ -2,7 +2,15 @@ it('duplicate pipeline', () => {
     cy.authAdminConsoleDevAdmin();
     const tmout = 20000;
     cy.get('#cloudProcessing > a', { timeout: tmout }).click()
-    cy.get('#bg-list', { timeout: tmout }).find('tr', { timeout: tmout }).its('length').as('initialRowCount')
+    cy.get('#bg-list', { timeout: tmout })
+        .find('tr', { timeout: tmout })
+        .its('length')
+        .as('rowCount');
+    let initialRowCount;
+    cy.get('@rowCount', { timeout: tmout }).then((rowCount) => {
+        initialRowCount = rowCount;
+    })
+
     cy.get(':nth-child(1) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
     cy.get(':nth-child(1) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
     let pipelineName;
@@ -12,7 +20,7 @@ it('duplicate pipeline', () => {
         cy.get('.close').click();
         cy.get(':nth-child(1) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
         cy.get(':nth-child(1) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(2) > a', { timeout: tmout }).click()
-        cy.get('#updateBg').click();
+        cy.get('#updateBg').scrollIntoView().click();
         cy.reload();
         cy.get(':nth-child(2) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
         cy.get(':nth-child(2) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(1) > a', { timeout: tmout }).click()
@@ -23,9 +31,8 @@ it('duplicate pipeline', () => {
     cy.reload();
     cy.get(':nth-child(2) > :nth-child(6) > .dropdown', { timeout: tmout }).click()
     cy.get(':nth-child(2) > :nth-child(6) > .dropdown > .dropdown-menu > :nth-child(3) > a > .text-danger', { timeout: tmout }).click()
-    cy.get('@initialRowCount', { timeout: tmout }).then((initialRowCount) => {
-        cy.get('#bg-list', { timeout: tmout })
-            .find('tr', { timeout: tmout })
-            .should('have.length', initialRowCount, { timeout: tmout })
+    cy.wait(2000);
+    cy.get('@rowCount', { timeout: tmout }).then((rowCount) => {
+        expect(initialRowCount).to.equal(rowCount);
     })
 })
