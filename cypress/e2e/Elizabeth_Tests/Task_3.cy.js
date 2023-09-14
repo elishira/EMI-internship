02039a8e4b7067b8972b8582ed074d231e885b29
elishira <97641486+elishira@users.noreply.github.com>
2023-09-14 00:41:07 -0700

    Update Task_3.cy.js

--- cypress/e2e/Elizabeth_Tests/Task_3.cy.js
+++ cypress/e2e/Elizabeth_Tests/Task_3.cy.js
@@ -1,4 +1,4 @@
-it('test change photo in kiosk', () => {
+it('Task_3 (test change photo in kiosk)', () => {
     cy.on('uncaught:exception', () => { cy.wait(1000); });
     cy.authAdminConsole();
     const tmout = 15000;
@@ -14,8 +14,9 @@ it('test change photo in kiosk', () => {
     })
     cy.visit('https://dev.storibox.com/kiosk/index.html?kType=evSB4&eId=ecd3cc00-dcd9-11ec-bfc4-89990b079b6d&pvId=ecd3cc02-dcd9-11ec-bfc4-89990b079b6d&title=Test&kioskId=devtest&formPos=50&pageTimeoutInterval=0&kps=280', { timeout: tmout })
         cy.get('#scanCardInput').type('MOPF-OKLQ-1222')
+        
         cy.get('#dScanning').click()
-        cy.get('.kiosk-action-buttons').click()
+        cy.get('.kiosk-action-buttons', { timeout: tmout }).click()
             .get('#packageBtn_2', { timeout: tmout }).click()
             .get('#customPopUpBody > .btn', { timeout: tmout }).click()
             .get('#img_6114023', { timeout: tmout }).click()
